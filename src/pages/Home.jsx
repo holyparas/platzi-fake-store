b6a521cc2e38744b140aac8acf4bae66f9dc91e5
Paras Gathani <gathanip@gmail.com>
2025-07-19 16:42:36 +0700

    add img to Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Header from '../components/Header';
+import img from "/shopping.jpg";
+
 const Home = () => {
   return (
     <div>
       <main className='homepage'>
         <h1>One stop shop for all your shopping needs!</h1>
-        <button>Shop Now</button>
+        <img src={img} alt="" className="homeImage"/>
       </main>
 
     </div>
