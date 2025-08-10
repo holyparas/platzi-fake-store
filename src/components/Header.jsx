1c3e4e456b21645b12f33a706647a5372ec8f9c9
Paras Gathani <gathanip@gmail.com>
2025-08-10 15:53:59 +0700

    cart counter doesnt show if 0

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,33 @@
-import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import { ShoppingCart, Store } from 'lucide-react';
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import { ShoppingCart, Store } from "lucide-react";
 import styles from "./Header.module.css";
 
-const Header = ({cart}) => {
+const Header = ({ cart }) => {
   const location = useLocation();
 
   return (
     <header className={styles.header}>
       <h1>
-        <Link to="/" className={styles.navLink}>Fake Store</Link>
+        <Link to="/" className={styles.navLink}>
+          Fake Store
+        </Link>
         <Store size={28} />
       </h1>
       <nav className={styles.navLinks}>
         <Link
           to="/"
-          className={`${styles.navLink} ${location.pathname === "/" ? styles.active : ""}`}
+          className={`${styles.navLink} ${
+            location.pathname === "/" ? styles.active : ""
+          }`}
         >
           Home
         </Link>
         <Link
           to="/products"
-          className={`${styles.navLink} ${location.pathname === "/products" ? styles.active : ""}`}
+          className={`${styles.navLink} ${
+            location.pathname === "/products" ? styles.active : ""
+          }`}
         >
           Products
         </Link>
@@ -29,10 +35,10 @@ const Header = ({cart}) => {
       <div className={styles.checkout}>
         <ShoppingCart size={22} />
         <Link to="/checkout">Checkout</Link>
-        <p>{cart.length}</p>
+        <p>{cart.length > 0 ? cart.length : ""}</p>
       </div>
     </header>
   );
-}
+};
 
 export default Header;
