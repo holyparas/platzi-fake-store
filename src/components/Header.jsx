081a74def097f19b97dcc570aa069aeae1e87519
Paras Gathani <gathanip@gmail.com>
2025-08-29 12:41:16 +0700

    modify header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,20 +7,15 @@ const Header = ({ cart }) => {
   const location = useLocation();
 
   return (
-    <header className={styles.header}>
-      <h1>
-        <Link to="/" className={styles.navLink}>
+    <header className="flex justify-between items-center bg-gray-900 text-gray-100">
+      <h1 className="flex gap-2 items-center">
+        <Link to="/" className="text-3xl">
           Fake Store
         </Link>
-        <Store size={28} />
+        <Store size={24} />
       </h1>
-      <nav className={styles.navLinks}>
-        <Link
-          to="/"
-          className={`${styles.navLink} ${
-            location.pathname === "/" ? styles.active : ""
-          }`}
-        >
+      <nav className="flex gap-5">
+        <Link to="/" className="text-3xl">
           Home
         </Link>
         <Link
@@ -38,6 +33,37 @@ const Header = ({ cart }) => {
         <p>{cart.length > 0 ? cart.length : ""}</p>
       </div>
     </header>
+    // <header className={styles.header}>
+    //   <h1>
+    //     <Link to="/" className={styles.navLink}>
+    //       Fake Store
+    //     </Link>
+    //     <Store size={28} />
+    //   </h1>
+    //   <nav className={styles.navLinks}>
+    //     <Link
+    //       to="/"
+    //       className={`${styles.navLink} ${
+    //         location.pathname === "/" ? styles.active : ""
+    //       }`}
+    //     >
+    //       Home
+    //     </Link>
+    //     <Link
+    //       to="/products"
+    //       className={`${styles.navLink} ${
+    //         location.pathname === "/products" ? styles.active : ""
+    //       }`}
+    //     >
+    //       Products
+    //     </Link>
+    //   </nav>
+    //   <div className={styles.checkout}>
+    //     <ShoppingCart size={22} />
+    //     <Link to="/checkout">Checkout</Link>
+    //     <p>{cart.length > 0 ? cart.length : ""}</p>
+    //   </div>
+    // </header>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,17 @@
-import Header from '../components/Header';
+import Header from "../components/Header";
 import img from "/shopping.jpg";
 
 const Home = () => {
   return (
     <div>
-      <main className='homepage'>
-        <h1>One stop shop for all your shopping needs!</h1>
-        <img src={img} alt="" className="homeImage"/>
+      <main className="homepage">
+        <h1 className="bg-gray-800 text-gray-100">
+          One stop shop for all your shopping needs!
+        </h1>
+        <img src={img} alt="" className="homeImage" />
       </main>
-
     </div>
   );
-}
+};
 
 export default Home;
